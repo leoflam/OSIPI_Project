d68e76d919ab5c6bbe6770dd308e0bff7795ff94
leoflam <leonardo.flammini@takeda.com>
2024-09-03 09:30:53 +0200

    [edit]

--- js/script.js
+++ js/script.js
@@ -16,24 +16,24 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Funzione per fare la chiamata API
-    function fetchData() {
-        const url = 'https://glosipi-web.man.aws.takeda.io/piwebapi/dataservers/F1DS03F4Hfeqh0G3eDN9d3ldEQV1VTVkdBUElBUkNQMDAx/points?nameFilter=RI*CP*BATCH*';
+    // function fetchData() {
+    //     const url = 'https://glosipi-web.man.aws.takeda.io/piwebapi/dataservers/F1DS03F4Hfeqh0G3eDN9d3ldEQV1VTVkdBUElBUkNQMDAx/points?nameFilter=RI*CP*BATCH*';
 
-        fetch(url, { mode: 'no-cors' })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-            })
-            .catch(error => {
-                console.error('There has been a problem with your fetch operation:', error);
-            });
-    }
+    //     fetch(url, { mode: 'no-cors' })
+    //         .then(response => {
+    //             if (!response.ok) {
+    //                 throw new Error('Network response was not ok ' + response.statusText);
+    //             }
+    //             return response.json();
+    //         })
+    //         .then(data => {
+    //             console.log(data);
+    //         })
+    //         .catch(error => {
+    //             console.error('There has been a problem with your fetch operation:', error);
+    //         });
+    // }
 
     // Chiamata della funzione fetchData
-    fetchData();
+    // fetchData();
 });
