6b887e20790e7eb14b592e6fc04f5c5532fe349f
leoflam <leonardo.flammini@takeda.com>
2024-09-12 15:05:37 +0200

    [12 set] 15:05

--- js/apiRequest.js
+++ js/apiRequest.js
@@ -1,13 +1,27 @@
-function apiRequestAccess(){
-    AuthorizationServiceConfiguration.fetchFromIssuer(openIdConnectUrl)
-  .then(response => {
-    log('Fetched service configuration', response);
-    this.configuration = response;
-    this.showMessage('Completed fetching configuration');
-  })
-  .catch(error => {
-    log('Something bad happened', error);
-    this.showMessage(`Something bad happened ${error}`)
-  });
-    
-}
+function apiRequest() {
+    const token = '';
+const apiUrl = 'https://glosipi-web.man.aws.takeda.io/piwebapi/dataservers/F1DS03F4Hfeqh0G3eDN9d3ldEQV1VTVkdBUElBUkNQMDAx/points?nameFilter=RI*CP*BATCH*';
+
+fetch(apiUrl, {
+    method: 'GET',
+    headers: {
+        'Authorization': `Bearer ${token}`,
+        'Accept': 'application/json',
+        
+    },
+    mode: 'no-cors'
+})
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+})
+.then(data => {
+    console.log('API response:', data);
+})
+.catch(error => {
+    console.error('Something bad happened:', error);
+});
+
+};
