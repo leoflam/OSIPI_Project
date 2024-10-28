af8cb1b2b5bc48e15f79cb09decb4cc16debe680
leoflam <leonardo.flammini@takeda.com>
2024-10-28 17:55:42 +0100

    fuck

--- js/cips.js
+++ js/cips.js
@@ -1,9 +1,9 @@
 const machines = [
-    { name: 'RI_N_0677_CP01F%BATCH', maintenance: 1, value: "", timeStamp: "2024-09-03T09:31:15Z", avgTime: "5400" , step:{name:"", timeRemaining: 0}},
-    { name: 'RI_N_0677_CP02F%BATCH', maintenance: 0, value: '', timeStamp: "2024-09-10T06:00:52Z", avgTime: "3600" , step:{name:"", timeRemaining:0 }},
-    { name: 'RI_N_0677_CP03F%BATCH', maintenance: 0, value: '', timeStamp: "2024-09-10T12:45:48Z", avgTime: "1800" , step:{name:"", timeRemaining: 0}},
-    { name: 'RI_N_0677_CP01F%BATCH', maintenance: 0, value: "", timeStamp: "2024-09-10T09:31:15Z", avgTime: "2400" , step:{name:"", timeRemaining: 0}},
-    { name: 'RI_N_0677_CP02F%BATCH', maintenance: 0, value: '', timeStamp: "2024-09-10T12:00:52Z", avgTime: "3600" , step:{name:"", timeRemaining: 0}},
-    { name: 'RI_N_0677_CP03F%BATCH', maintenance: 0, value: "S1_030924_1332", timeStamp: "2024-09-10T14:27:48Z", avgTime: "3600" , step:{name:"RO", timeRemaining: 600}},
+    { name: 'RI_N_0677_CP01F%BATCH', maintenance: 1, value: "", timeStamp: "2024-10-28T16:18:00Z", avgTime: "5400", step: { name: "", timeRemaining: 0 } },
+    { name: 'RI_N_0677_CP02F%BATCH', maintenance: 0, value: "S1_281024_3945", timeStamp: "2024-10-28T06:00:52Z", avgTime: "3600", step: { name: "RO", timeRemaining: 0 } },
+    { name: 'RI_N_0677_CP03F%BATCH', maintenance: 0, value: "", timeStamp: "2024-10-28T12:45:48Z", avgTime: "1800", step: { name: "ACID", timeRemaining: 1278 } },
+    { name: 'RI_N_0677_CP04F%BATCH', maintenance: 0, value: "", timeStamp: "2024-10-28T09:31:15Z", avgTime: "2400", step: { name: "", timeRemaining: 0 } },
+    { name: 'RI_N_0677_CP05F%BATCH', maintenance: 0, value: "S1_281024_4439", timeStamp: "2024-10-28T15:00:52Z", avgTime: "3600", step: { name: "RO2", timeRemaining: 541 } },
+    { name: 'RI_N_0677_CP06F%BATCH', maintenance: 0, value: "S1_281024_2196", timeStamp: "2024-10-28T14:27:48Z", avgTime: "3600", step: { name: "WFI", timeRemaining: 600 } },
     // Altri oggetti macchina...
 ];
